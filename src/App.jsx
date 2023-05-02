fb6d621a77a3fdad0f7f812a5cac5db0ba589d36
LipeUNIM <86551927+Moschindinho@users.noreply.github.com>
2023-05-02 22:35:41 +0100

    Added my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog"
 import Podcast from "./pages/Podcast"
 import Donate from "./pages/Donate"
 import Footer from "./components/Footer/Footer";
-import { useRef, useEffect } from "react";
-import logo from "./assets/img/preload.svg"
 
 const App = () => {
-  const ref = useRef(null)
-  useEffect(() => {
-    const preloader = ref.current
-    window.addEventListener("load", function () {
-      preloader.classList.add("loaded");
-      document.body.classList.add("loaded");
-    });
-  }, []);
   return (
     <>
-      <div className="preload" ref={ref}>
-        <img src={logo} alt="loader" />
-      </div>
       <Navbar />
       <Routes>
         <Route element={<Home/>} path="/"/>
